97e25e5ad11352c9d934eb56e8e64891bd54bb9a
Edcyril Manigos <162562445+Xyrildocs@users.noreply.github.com>
2024-06-12 18:49:21 +0800

    Add files via upload

---
+++ notification-staff.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="STYLES/notification-staff.css">
+	<title></title>
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
+    crossorigin="anonymous" 
+    referrerpolicy="no-referrer"/>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+  <nav class="navbar">
+    <div class="navdiv">
+        <div class="logo">
+            <span class="ustp">USTP</span>
+            <div class="role-box">
+                 <span class="role">STAFF</span>
+            </div>
+        </div>
+        <ul>
+          <li><a href="notification-staff.php"> <i class="fa-solid fa-bell"></i> NOTIFICATIONS</a></li>
+          <li><a href="home-staff.php"> <i class="fa-solid fa-house"></i> HOME</a></li>
+          <li><a href="clearance-staff.php"> <i class="fa-solid fa-folder"></i> CLEARANCE</a></li>
+          <li>
+              <a href="#"> <i class="fa-solid fa-user"></i> USERS ˅ </a>
+              <ul class="dropdown">
+                  <li><a href = "update contact info-staff.php"> UPDATE CONTACT INFO </a></li>
+                  <li><a href = "change password-staff.php"> CHANGE PASSWORD </a></li>
+                  <li><a href = "index.php"> LOGOUT </a></li>
+              </ul>
+          </li>
+      </ul>
+    </div>
+  </nav>
+
+  <h1>Notification</h1>
+  <table>
+    <tr>
+      <th>Date & Time</th>
+      <th class="Messages">Message</th>
+    </tr>
+  </table>
+  
+    <div class="Develop">
+      <p>
+        Develop by:<span class="MACC"> MACC</span>
+      </p>
+    </div>
+
+</body>
+</html>
