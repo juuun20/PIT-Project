4625c90654a635a26a03e1d39f24a0c45f8f986f
crysstel <162566749+crysstel@users.noreply.github.com>
2024-06-12 19:50:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="STYLES/index.css">
+	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>	
+
+<center>
+	<img src="img/background1.jpg" class="background">
+	
+	<div class="d1">
+		<img src="img/logo1.png">
+	</div>
+
+	<h1 class="p1">UNIVERSITY OF SCIENCE AND TECHNOLOGY OF SOUTHERN PHILIPPINES</h1>
+	<h1 class="p2">E-CLEARANCE SYSTEM</h1>
+	<h1 class="p3">OROQUIETA CAMPUS</h1>
+
+	<div class="container">
+        <img src="img/user.png" class="user1">
+		<h1 class="p10">USER ROLE</h1>
+    <div class="role">
+      <a href="login-admin.php">
+        <button value = "submit" type="submit" class="admin">ADMINISTRATOR</button>
+      </a>
+      <a href="login-staff.php">
+        <button value = "submit" type="submit" class="staff">STAFF</button><br>
+      </a>
+      <a href="login-faculty.php">
+        <button value = "submit" type="submit" class="faculty">FACULTY</button>
+      </a>
+      <a href="login-student.php">
+        <button value = "submit" type="submit" class="student">STUDENT</button>
+      </a>
+    </div>
+	</div>
+	</center>
+</body>
+</html>
